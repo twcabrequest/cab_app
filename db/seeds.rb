5ea0292a56ee997c5bc9d6584402b65eb136c40c
pulkitko@thoughtworks.com <pulkitko@thoughtworks.com>
2014-02-04 16:15:28 +0530

    modifying seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,4 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Admin.create( name: "pulkitko", contact_no: "8826811556", email: "pulkitko@thoughtworks.com" , status: true)
-Admin.create( name: "priyankd", contact_no: "8826811556", email: "pulkit@thoughtworks.com" , status: true)
-Admin.create( name: "ramankan", contact_no: "8826811556", email: "pulk@thoughtworks.com" , status: true)
-Vendor.create( name: "pulkitko", contact_no: "8826811556", email: "pulkitko@thoughtworks.com" , order: Vendor.all.count + 1)
+Admin.create( name: "ggnfacilities", contact_no: "8826811556", email: "ggnfacilities@thoughtworks.com" , status: true)
