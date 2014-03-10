1a17abeb276276a1a5a2226a3857920e32dc93a3
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-10 11:08:48 +0530

    adding okta_logout filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :okta_authenticate!
   #before_filter CASClient::Frameworks::Rails::Filter, :except => [:submit_response]
+  p :okta_user
   before_filter :is_admin, :only => [:edit,:index,:new,:show,:create,:update]
 
   def is_admin
