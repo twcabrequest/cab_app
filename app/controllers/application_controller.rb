dc684b34f579ae4f426604a322bc3277b28b7d04
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-11 14:55:40 +0530

    including session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :okta_authenticate!
   #before_filter CASClient::Frameworks::Rails::Filter, :except => [:submit_response]
-  p current_user
+  p SessionHelper.get(:current_user)
   before_filter :is_admin, :only => [:edit,:index,:new,:show,:create,:update]
 
   def is_admin
-    @is_admin = Admin.pluck(:name).include? current_user
+    @is_admin = Admin.pluck(:name).include? `current_user`
+  end
+
+  def deroy
+
   end
 
 end
