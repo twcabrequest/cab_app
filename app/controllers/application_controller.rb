200f6aecbc62ae13f53bdbfbb5a5bc88c5794057
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-11 14:33:33 +0530

    including session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
+require 'okta_saml/session_helper'
 class ApplicationController < ActionController::Base
-  include OktaSaml::SessionHelper
   protect_from_forgery
   before_filter :okta_authenticate!
   #before_filter CASClient::Frameworks::Rails::Filter, :except => [:submit_response]
