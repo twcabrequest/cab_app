57760e14c13ca5019aeab548ccb9eaf1d8f37dfd
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-11 16:30:16 +0530

    using remote-pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :okta_authenticate!
   #before_filter CASClient::Frameworks::Rails::Filter, :except => [:submit_response]
-  #include 'okta_saml/session_helper'
   before_filter :is_admin, :only => [:edit,:index,:new,:show,:create,:update]
 
   def is_admin
     @is_admin = Admin.pluck(:email).include? @current_user.email
-    p ">>>>>>>>>>>>>", @current_user.methods.join(",")
-    begin
-    p ">>>>>>>>>>>>>>>" , @current_user.firstName
-    rescue => ex
-      p ">>>>>>>>>ERROR", ex.message
-    end
+    require 'pry-remote'; binding.remote_pry
   end
-
-  def self.deroy
-    require 'pry';binding.pry
-  end
-
 end
