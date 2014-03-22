ca9fde7341f2a80c23e49bcce910d7dd8c23431f
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-22 10:41:36 +0530

    adding logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
   def is_admin
     @is_admin = Admin.pluck(:email).include? @current_user.email
     session[:okta_user] = @current_user.email.partition('@').first
-    p session[:requesters_logout_path]+'----'+session[:requesters_logout_url]+'------'+session[:redirect_url]
+    p session[:requesters_logout_path]
+    p session[:requesters_logout_url]
+    p session[:redirect_url]
   end
 end
