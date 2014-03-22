cef0b2b459d366d514a7670803f7e6f21f2be82c
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-22 13:59:33 +0530

    adding logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,9 @@
 class ApplicationController < ActionController::Base
   include OktaSaml::SessionHelper
   protect_from_forgery
-  before_filter :print_session
   before_filter :okta_authenticate!, :except => [:submit_response]
   before_filter :is_admin, :only => [:edit,:index,:new,:show,:create,:update]
 
-  def print_session
-    p "am here"
-    p request.to_s
-    p request.methods.to_a.to_s
-    p request.fullpath
-    #request.fullpath = '/support_centers/show'
-    #p session[:redirect_url]
-  end
   def is_admin
     p 'checkin admin'
     @is_admin = Admin.pluck(:email).include? @current_user.email
