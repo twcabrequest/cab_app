55d743cdf042890bcd71c03885071ba016432938
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-22 11:57:47 +0530

    adding logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@
 class ApplicationController < ActionController::Base
   include OktaSaml::SessionHelper
   protect_from_forgery
+  p "am here"
   before_filter :okta_authenticate!, :except => [:submit_response]
+  p 'reached here'
   #before_filter CASClient::Frameworks::Rails::Filter, :except => [:submit_response]
   before_filter :is_admin, :only => [:edit,:index,:new,:show,:create,:update]
 
   def is_admin
+    p 'checkin admin'
     @is_admin = Admin.pluck(:email).include? @current_user.email
     session[:okta_user] = @current_user.email.partition('@').first
     p session[:requesters_logout_path]
