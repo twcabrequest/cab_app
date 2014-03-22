0b13f473e1de5d0554622189cfcb477d74f62098
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-22 11:29:42 +0530

    adding logout

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -9,7 +9,7 @@ class RequestersController < ApplicationController
     p '+++++++++reached here+++++++++++++'
     #@current_user.methods
     sign_out
-    redirect_to '/'
+    redirect_to 'https://thoughtworks.oktapreview.com/login/signout'
     #cookies.delete(:remember_token)
     #@current_user = nil
     #CASClient::Frameworks::Rails::Filter.logout(self)
