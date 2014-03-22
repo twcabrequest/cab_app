cd15b4487965859b3ec43ea38147b9a6688c7c4e
Vaibhav <vaibhavk@thoughtworks.com>
2014-03-22 11:12:38 +0530

    adding logout

--- app/controllers/requesters_controller.rb
+++ app/controllers/requesters_controller.rb
@@ -9,7 +9,7 @@ class RequestersController < ApplicationController
     p '+++++++++reached here+++++++++++++'
     #@current_user.methods
 
-    cookies.delete(:remember_token)
+    #cookies.delete(:remember_token)
     @current_user = nil
     #CASClient::Frameworks::Rails::Filter.logout(self)
   end
